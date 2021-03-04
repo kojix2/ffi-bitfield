9e936e81d8236ea62eb2f5250ec6a9d2c94eaf9d
kojix2 <2xijok@gmail.com>
2021-03-04 14:40:37 +0900

    First commit

---
+++ lib/bitstruct.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "bitstruct/version"
+
+module Bitstruct
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/bitstruct/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Bitstruct
+  VERSION = "0.1.0"
+end
