8dd8bba52ee6aa4dc15667bbb49de0eb86aaf728
kojix2 <2xijok@gmail.com>
2021-03-17 11:06:41 +0900

    Add multiple fields test

--- test/bitstruct_test.rb
+++ test/bitstruct_test.rb
@@ -17,10 +17,16 @@ class BitstructTest < Minitest::Test
               :a5, 1,
               :a6, 1,
               :a7, 1
+
+    bitfields :b,
+              :b0, 1,
+              :b1, 1,
+              :b2, 2,
+              :b3, 4
   end
 
   256.times do |i|
-    define_method("test_#{i}") do
+    define_method("test_a_#{i}") do
       s = Struct1.new
       s[:a] = i
       v = s[:a0] +
@@ -31,6 +37,18 @@ class BitstructTest < Minitest::Test
           s[:a5] * 32 +
           s[:a6] * 64 +
           s[:a7] * 128
+      assert_equal i, v
+    end
+  end
+
+  256.times do |i|
+    define_method("test_b_#{i}") do
+      s = Struct1.new
+      s[:b] = i
+      v = s[:b0] +
+          s[:b1] * 2 +
+          s[:b2].to_s(2).reverse.each_char.map.with_index{|v, i| v.to_i * (2**(i+2))}.inject(:+) +
+          s[:b3].to_s(2).reverse.each_char.map.with_index{|v, i| v.to_i * (2**(i+4))}.inject(:+)
       assert_equal i, v
     end
   end
