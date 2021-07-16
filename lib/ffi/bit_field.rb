22057f15f6d2ca76158ff4bf9d9b2cbc3f9f59b0
kojix2 <2xijok@gmail.com>
2021-07-16 17:03:35 +0900

    Fix ruby script file path

--- lib/ffi/bit_field.rb
+++ lib/ffi/bit_field.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require_relative 'bit_field/version'
-require_relative 'bit_field/bit_field_supporter'
 require_relative 'bit_struct'
 require_relative 'managed_bit_struct'
 
--- lib/ffi/managed_bit_struct.rb
+++ lib/ffi/managed_bit_struct.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require 'ffi'
-require_relative 'bit_struct/version'
+require_relative 'bit_field/version'
 require_relative 'bit_field/layout'
 require_relative 'bit_field/property'
 
